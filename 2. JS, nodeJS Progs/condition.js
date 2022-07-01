b4925ed6c92c6306896619a0e36e3c0ddd3190a8
jenilshingala <107799839+jenilshingala@users.noreply.github.com>
2022-07-01 21:48:08 +0530

    Add files via upload

---
+++ 2. JS, nodeJS Progs/condition.js
@@ -0,0 +1,9 @@
+var a=10;
+var b=20;
+var c = a+b;
+
+if(c==30) {
+    console.log("\nc is 30\n");
+} else {
+    console.log("\nc is not 30\n");
+}
---
+++ 2. JS, nodeJS Progs/expression.js
@@ -0,0 +1,11 @@
+var a=10;
+var b=20;
+
+console.log("\na: "+ a + " - b: " + b);
+console.log(`a: ${a} - b: ${b}\n`);   //ES6 syntax
+
+var c = a+b;
+
+console.log("sum = "+ c);
+console.log(`sum = ${c}\n`);
+// same goes with - * / ** etc etc 
